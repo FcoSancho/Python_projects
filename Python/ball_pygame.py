580a4ea3c3c9cb54a38061049e7c21c2c5d12fd3
Francisco Sancho <60715437+FcoSancho@users.noreply.github.com>
2020-05-02 19:11:58 -0600

    Juego Bola

--- Python/ball_pygame.py
+++ Python/ball_pygame.py
@@ -1,4 +1,5 @@
 import sys, pygame
+
 #Inicializamos pygame
 pygame.init()
 #Hacemos la ventana
@@ -11,9 +12,15 @@ pygame.display.set_caption("Juego BALL")
 #width, height = 800, 600
 speed = [1, 1]
 white = 255, 255, 255
-#Crear objeto
-ball = pygame.image.load("ball.jpg")
+#Crear objeto pelota
+ball = pygame.image.load("ball2.jpg")
 ballrect = ball.get_rect()
+#Crear objeto bate
+bate = pygame.image.load("barra-lateral-roja2.png")
+baterect = bate.get_rect() 
+#Poner el bate en el medio de la pantalla
+baterect.move_ip(950, 300)
+
 #Comenzamos bucle del juego
 run = True
 while run:
@@ -22,16 +29,27 @@ while run:
   #Captura de eventos producidos
     for event in pygame.event.get():
         if event.type == pygame.QUIT: run = False
-  #Muevo la pelota
-    ballrect = ballrect.move(speed)
-    #Compruebo que la pelota llega a los lìmites de la ventaa
-    if ballrect.left < - 25 or ballrect.right > width + 25:
-        speed[0] = -speed[0]
-    if ballrect.top < - 25 or ballrect.bottom > height + 25:
-        speed[1] = -speed[1]
-    #Hacer fondo blanco y dibujar la pelota y se actualiza la pantalla
-    screen.fill(white)
-    screen.blit(ball, ballrect)
-    pygame.display.flip()
+#Movimiento con las teclas
+keys = pygame.key.get_pressed()
+if keys[pygame.K_UP]:
+  baterect = baterect.move(0, -1)
+if keys[pygame.K_DOWN]:
+  baterect = baterect.move(0, 1)
+#Compruebo la colisión
+if baterect.colliderect(ballrect):
+  speed[0] = -speed[0]
+#Muevo la pelota
+ballrect = ballrect.move(speed)
+#Compruebo que la pelota llega a los lìmites de la ventana
+if ballrect.left < - 25 or ballrect.right > width + 25:
+    speed[0] = -speed[0]
+if ballrect.top < - 25 or ballrect.bottom > height + 25:
+    speed[1] = -speed[1]
+#Hacer fondo blanco y dibujar la pelota y se actualiza la pantalla
+screen.fill(white)
+screen.blit(ball, ballrect)
+#Dibujando el bate
+screen.blit(bate, baterect)
+pygame.display.flip()
 #Salir del juego
 pygame.quit()
